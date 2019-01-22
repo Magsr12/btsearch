173d69a25e7637e2dfb6ac1c541bbd6098253234
richardparker6103 <richardparker6103@mailinator.com>
2019-01-22 11:07:29 -0300

    Update btsearch.py

--- btsearch.py
+++ btsearch.py
@@ -192,10 +192,10 @@ def main(repeat=False):
 +------+----------+
 
 from prettytable import PrettyTable
->>> t = PrettyTable(['Name', 'Age'])
->>> t.add_row(['Alice', 24])
->>> t.add_row(['Bob', 19])
->>> print t
+ t = PrettyTable(['Name', 'Age'])
+ t.add_row(['Alice', 24])
+ t.add_row(['Bob', 19])
+ print t
 +-------+-----+
 |  Name | Age |
 +-------+-----+
