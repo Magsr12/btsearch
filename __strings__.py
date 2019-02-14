2141834f44fb7c763d5e3785e3ef9948dde06f4d
richardparker6103 <richardparker6103@mailinator.com>
2019-02-14 18:05:38 -0200

    Updated and added README.txt

---
+++ __strings__.py
@@ -0,0 +1,11 @@
+k_titles = []
+k_sizes = []
+k_seeders = []
+k_magnets = []
+k_links = []
+t_seeders = []
+t_titles = []
+t_sizes = []
+t_magnets = []
+
+PAGE_RANGE = 4 # CHANGE THIS TO ADD MORE PAGES IN SEARCHING
