2feb88b0eb28aee1ddeca06ad9e970e14e004065
richardparker6103 <richardparker6103@mailinator.com>
2019-02-14 18:06:10 -0200

    Delete __colors__.py

--- __colors__.py
+++
@@ -1,9 +0,0 @@
-import colorama
-colorama.init()
-
-BOLD = '\033[1m'
-CYAN = BOLD + '\033[36m'
-NORMAL = BOLD + '\033[37m'
-YELLOW = BOLD + '\033[33m'
-RED = BOLD + '\033[31m'
-GREEN = BOLD + '\033[32m'
