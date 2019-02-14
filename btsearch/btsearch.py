a7ca19e9ef85c6e1e152de7a26dba5078c0deadf
richardparker6103 <richardparker6103@mailinator.com>
2019-02-14 13:35:03 -0200

    Updated

--- btsearch/btsearch.py
+++ btsearch/btsearch.py
@@ -5,22 +5,9 @@ import argparse
 
 if len(sys.argv) < 2:
 	exit('[*] Uso: python btsearch.py {SEARCH}')
-for a in sys.argv:
-	if a == sys.argv[0]:
-		pass
-	else:
-		if int(PAGE_RANGE) > 6:
-			exit('[*] Numero maximo de paginas excedido, valor maximo: 6')
-		'''
+if int(PAGE_RANGE) > 6:
+    exit('[*] Numero maximo de paginas excedido, valor maximo: 6')
 
-		if a == '-r':
-			global PAGE_RANGE
-			NEW_RANGE = sys.argv[value]
-			if int(NEW_RANGE) > 5:
-				
-			print '[*] Numero de paginas: ' + str(NEW_RANGE)
-			PAGE_RANGE = int(NEW_RANGE)
-		'''
 init()
 
 
