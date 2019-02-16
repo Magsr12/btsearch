05e541f116ef082e789734e747f931e843ac2427
richardparker6103 <richardparker6103@mailinator.com>
2019-02-16 17:52:28 -0200

    Update __strings__.py

--- btsearch/__strings__.py
+++ btsearch/__strings__.py
@@ -1,3 +1,4 @@
+PAGE_RANGE = 4 # CHANGE THIS TO ADD MORE PAGES IN SEARCHING
 k_titles = []
 k_sizes = []
 k_seeders = []
@@ -13,4 +14,4 @@ x_seeders = []
 x_magnets = []
 x_links = []
 temp_list = []
-PAGE_RANGE = 4 # CHANGE THIS TO ADD MORE PAGES IN SEARCHING
+
