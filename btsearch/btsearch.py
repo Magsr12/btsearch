517dbe5bbde9986cd541185c1abb4b222b721c80
richardparker6103 <richardparker6103@mailinator.com>
2019-02-27 17:06:28 -0300

    Add files via upload

--- btsearch/btsearch.py
+++ btsearch/btsearch.py
@@ -29,16 +29,27 @@ except ImportError:
     print '[*] colorama nao instalado, tentando instalar...'
     os.system('pip install colorama')
 
-if len(sys.argv) < 2:
-	exit(YELLOW + '[*] Uso: python btsearch.py ' + NORMAL + '"SEARCH"')
-if '--help' in sys.argv or '-h' in sys.argv:
-    exit(YELLOW + '''Uso: python btsearch.py "SEARCH" [-r 6] [-q tpb,kickass]\n''' + NORMAL +
-'''
+usage = RED + '''
+888888b. 88888888888 .d8888b.  8888888888        d8888 8888888b.   .d8888b.  888    888 
+888  "88b    888    d88P  Y88b 888              d88888 888   Y88b d88P  Y88b 888    888 
+888  .88P    888    Y88b.      888             d88P888 888    888 888    888 888    888 
+8888888K.    888     "Y888b.   8888888        d88P 888 888   d88P 888        8888888888 
+888  "Y88b   888        "Y88b. 888           d88P  888 8888888P"  888        888    888 
+888    888   888          "888 888          d88P   888 888 T88b   888    888 888    888 
+888   d88P   888    Y88b  d88P 888         d8888888888 888  T88b  Y88b  d88P 888    888 
+8888888P"    888     "Y8888P"  8888888888 d88P     888 888   T88b  "Y8888P"  888    888
+''' + YELLOW + '\nUso: python btsearch.py "SEARCH"'+ NORMAL + ' [-r 6] [-q tpb,kickass]\n' + '''
 -q          Servico de pesquisa, disponiveis: tpb, kickass, 1337x.
 -r          Numero maximo de paginas a serem vasculhadas, default=4.
 --movies    Procura pela query em sites nao oficiais, pode levar mais tempo do que o normal.
 --verbose   Habilita a depuracao, somente disponivel em --movies.
-''')
+'''
+
+if len(sys.argv) < 2:
+    exit(usage)
+    if '--help' in sys.argv or '-h' in sys.argv:
+        exit(usage)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('"SEARCH"')
